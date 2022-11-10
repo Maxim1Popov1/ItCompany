8f3368f0b6e606d7844aa5063b44f923f559042c
unknown <89188599154B@gmail.com>
2022-11-10 16:10:32 +0300

    full fix

--- itCompanyNext.js
+++ itCompanyNext.js
@@ -5,8 +5,9 @@ class Employ {
     this.side = settings.side;
     this.tasks = [];
   }
+
   createEmploy() {
-    const devGrade = gradeGenetation();
+    const devGrade = Util.gradeGenetation();
     let devRate = 0;
     const DEVELOPER_GRADES = {
       junior: { grade: "Junior", rate: 12 },
@@ -49,24 +50,25 @@ class Customer {
     this.projectSettings = [];
   }
   createProject() {
-    const RANDOM_GENERATION_TASK_SETTINGS = { min: 1, max: 6 };
+    const RANDOM_GENERATION_TASK_SETTINGS = { min: 1, max: 10 };
     const RANDOM_GENERATION_ESTIMATION_SETTINGS = { min: 1, max: 30 };
-    const numberOfTasks = randomGeneration(
+    const numberOfTasks = Util.randomGeneration(
       RANDOM_GENERATION_TASK_SETTINGS.min,
       RANDOM_GENERATION_TASK_SETTINGS.max
     );
-    for (let i = 1; i < numberOfTasks; i++) {
+
+    const numberOfTaskArr = Array(numberOfTasks).fill();
+    numberOfTaskArr.forEach((number, i) => {
       const task = {
-        description: `Task ${i}`,
-        side: sideGenetation(),
-        estimate: randomGeneration(
+        description: `Task ${i + 1}`,
+        side: Util.sideGenetation(),
+        estimate: Util.randomGeneration(
           RANDOM_GENERATION_ESTIMATION_SETTINGS.min,
           RANDOM_GENERATION_ESTIMATION_SETTINGS.max
         ),
       };
-
       this.projectSettings.push(task);
-    }
+    });
 
     return this.projectSettings;
   }
@@ -76,102 +78,137 @@ class ItCompany {
   employees = [];
 
   hireEmployees() {
-    const RANDOM_GENERATION_DEVS = { min: 1, max: 3 };
-    const devs = randomGeneration(
+    const RANDOM_GENERATION_DEVS = { min: 1, max: 4 };
+    const devs = Util.randomGeneration(
       RANDOM_GENERATION_DEVS.min,
       RANDOM_GENERATION_DEVS.max
     );
-
     const employArr = Array(devs).fill();
     employArr.forEach((employ) => {
       const frontEmploy = new FrontendDeveloper();
       const backEmploy = new BackendDeveloper();
-      let frontCreation = frontEmploy.createEmploy();
-      let backCreation = backEmploy.createEmploy();
+      const frontCreation = frontEmploy.createEmploy();
+      const backCreation = backEmploy.createEmploy();
       this.employees.push(frontCreation);
       this.employees.push(backCreation);
     });
   }
 
   calculateProjectImplementation(project) {
-    console.log("project :>> ", project);
-    console.log("this.employees :>> ", this.employees);
-    const assignTask = project.map((task, index) => {
-      let indexOfTask = 0;
+    const DEVELOPER_GRADES = {
+      junior: { grade: "Junior", rate: 12, efficiency: 1.5 },
+      middle: { grade: "Middle", rate: 22, efficiency: 1.3 },
+      signor: { grade: "Signor", rate: 29, efficiency: 1 },
+    };
+
+    const findExistTask = (taksId) => {
+      const finded = this.employees.find((employ) => {
+        return employ.tasks.find((task) => task === taksId);
+      });
+      return !!finded;
+    };
+
+    project.forEach((task, index) => {
       this.employees.forEach((employ) => {
-        if (task.side === employ.side) {
-          indexOfTask = index + 1;
-          employ.tasks.push(indexOfTask);
-          return;
+        const finded = findExistTask(index + 1);
+        if (
+          employ.side === task.side &&
+          finded === false &&
+          employ.tasks.length === 0
+        ) {
+          employ.tasks.push(index + 1);
         }
       });
     });
 
-    let estimation = 0;
+    const projectCost = this.employees.reduce((sumEmploy, employ) => {
+      const sumOfTasks = employ.tasks.reduce((sumTask, task) => {
+        const findedTask = project.find(
+          (item, indexOfTask) => task === indexOfTask + 1
+        );
 
-    const employCfg = this.employees.map((employ) => {
-      let sum = 0;
+        if (!!findedTask) {
+          switch (employ.grade) {
+            case DEVELOPER_GRADES.junior.grade:
+              sumTask =
+                DEVELOPER_GRADES.junior.efficiency *
+                DEVELOPER_GRADES.junior.rate *
+                findedTask.estimate;
+              break;
+            case DEVELOPER_GRADES.middle.grade:
+              sumTask =
+                DEVELOPER_GRADES.middle.efficiency *
+                DEVELOPER_GRADES.middle.rate *
+                findedTask.estimate;
+              break;
+            case DEVELOPER_GRADES.signor.grade:
+              sumTask =
+                DEVELOPER_GRADES.signor.efficiency *
+                DEVELOPER_GRADES.signor.rate *
+                findedTask.estimate;
+              break;
+          }
+        }
+        return sumTask;
+      }, 0);
+      return sumEmploy + sumOfTasks;
+    }, 0);
 
-      employ.tasks.forEach((task) => {
-        sum = task.estimate + sum;
-      });
+    const projectEstimation = this.employees.reduce((sumEmploy, employ) => {
+      const employEstimate = employ.tasks.reduce((estimate, task) => {
+        const findedTask = project.find(
+          (item, indexOfTask) => task === indexOfTask + 1
+        );
 
-      if (employ.grade === "Junior") {
-        sum = sum * 1.5 * 12;
-      }
-      if (employ.grade === "Middle") {
-        sum = sum * 1.3 * 22;
-      }
-      if (employ.grade === "Signor") {
-        sum = sum * 29;
-      }
-      estimation = sum + estimation;
-    });
+        if (!!findedTask) {
+          switch (employ.grade) {
+            case DEVELOPER_GRADES.junior.grade:
+              estimate =
+                DEVELOPER_GRADES.junior.efficiency * findedTask.estimate;
+              break;
+            case DEVELOPER_GRADES.middle.grade:
+              estimate =
+                DEVELOPER_GRADES.middle.efficiency * findedTask.estimate;
+              break;
+            case DEVELOPER_GRADES.signor.grade:
+              estimate =
+                DEVELOPER_GRADES.signor.efficiency * findedTask.estimate;
+              break;
+          }
+        }
+        return estimate;
+      }, 0);
+      return sumEmploy + employEstimate;
+    }, 0);
 
-    let numberOfDays = 0;
-    const employCfgg = this.employees.map((employ) => {
-      let daySum = 0;
-
-      employ.tasks.forEach((task) => {
-        daySum = task.estimate + daySum;
-      });
-
-      if (employ.grade === "Junior") {
-        daySum = (daySum * 1.5) / 8;
-      }
-      if (employ.grade === "Middle") {
-        daySum = (daySum * 1.3) / 8;
-      }
-      if (employ.grade === "Signor") {
-        daySum = daySum / 8;
-      }
-
-      numberOfDays = daySum + numberOfDays;
-    });
-
-    //  console.log("numberOfDays-", numberOfDays, );
+    return {
+      projectEstimation,
+      projectCost,
+    };
   }
 }
 
-const randomGeneration = (max, min) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+class Util {
+  static randomGeneration = (max, min) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
 
-const randomBoolean = () => {
-  return Math.random() < 0.5;
-};
+  static randomBoolean = () => {
+    return Math.random() < 0.5;
+  };
 
-const gradeGenetation = () => {
-  const grades = ["Junior", "Middle", "Signor"];
-  const randomGrade = randomGeneration(3, 0);
-  return grades[randomGrade];
-};
+  static gradeGenetation = () => {
+    const grades = ["Junior", "Middle", "Signor"];
+    const randomGrade = Util.randomGeneration(3, 0);
+    return grades[randomGrade];
+  };
 
-const sideGenetation = () => {
-  const sides = ["Back", "Front"];
-  const randomSide = randomGeneration(2, 0);
-  return sides[randomSide];
-};
+  static sideGenetation = () => {
+    const sides = ["Back", "Front"];
+    const randomSide = Util.randomGeneration(2, 0);
+    return sides[randomSide];
+  };
+}
 
 const itCompany = new ItCompany();
 itCompany.hireEmployees();
@@ -179,4 +216,9 @@ const project = new Customer();
 project.createProject();
 const resultOfProjectImplementation = itCompany.calculateProjectImplementation(
   project.projectSettings
+);
+
+console.log(
+  "resultOfProjectImplementation :>> ",
+  resultOfProjectImplementation
 );
